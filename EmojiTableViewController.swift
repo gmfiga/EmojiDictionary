7bc3c241c3b3adac3f7bc61164936cf1a864dde2
Administrator <administrator@gbtc408013dt.montgomerycollege.edu>
2018-06-27 13:18:45 -0400

    now can delete

--- EmojiTableViewController.swift
+++ EmojiTableViewController.swift
@@ -71,6 +71,17 @@ class EmojiTableViewController: UITableViewController {
         tableView.reloadData()
     }
     
+    override func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCellEditingStyle {
+        return .delete
+    }
+ 
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete{
+            emojis.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+        }
+    }
+    
     
     // Override to support conditional editing of the table view.
     /*override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
